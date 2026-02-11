47b20702b5d8fb6eab7bcd8730fd5b1ce7f747a7
Lazularszz <thom576xd@gmail.com>
2026-02-11 16:35:38 +0100

    :green_heart: Correction de la configuration CI/CD

--- _00_ASBank2023/RunSonar.bat
+++ _00_ASBank2023/RunSonar.bat
@@ -1,1 +1,13 @@
-mvn clean verify sonar:sonar -Dsonar.projectKey=BANK_IUT_2023 -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_c55e85adf298ef5ffae40c13ec282dfc9482301b
+@echo off
+REM Chargement des variables depuis le fichier .env
+if exist .env (
+    for /f "tokens=1,* delims==" %%a in ('type .env') do (
+        set %%a=%%b
+    )
+) else (
+    echo Le fichier .env est introuvable.
+    exit /b 1
+)
+
+REM Lancement de Sonar avec les variables chargées
+mvn clean verify sonar:sonar -Dsonar.projectKey=%SONAR_PROJECT_KEY% -Dsonar.host.url=%SONAR_HOST_URL% -Dsonar.login=%SONAR_LOGIN%
