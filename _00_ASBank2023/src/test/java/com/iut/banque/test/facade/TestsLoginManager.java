1dbd8cd152d60af3e08f953b97faca4f10fe1fba
EskYzK <alexkerber54190@gmail.com>
2026-02-22 23:20:30 +0100

    :construction: Modifications des fichiers pour qualité de code

--- _00_ASBank2023/src/test/java/com/iut/banque/test/facade/TestsLoginManager.java
+++ _00_ASBank2023/src/test/java/com/iut/banque/test/facade/TestsLoginManager.java
@@ -76,29 +76,29 @@ public class TestsLoginManager {
         String newPwd = "new";
 
         // Création d'un utilisateur concret
-        Gestionnaire user = new Gestionnaire();
+        Gestionnaire user3 = new Gestionnaire();
         String hashedOld = PasswordHasher.hash(oldPwd);
-        user.setUserPwd(hashedOld);
+        user3.setUserPwd(hashedOld);
 
         // DAO mock
         IDao dao = mock(IDao.class);
-        doNothing().when(dao).updateUser(user);
+        doNothing().when(dao).updateUser(user3);
 
         // LoginManager réel
-        LoginManager manager = new LoginManager();
-        manager.setDao(dao);
+        LoginManager manager1 = new LoginManager();
+        manager1.setDao(dao);
 
         // Execution
-        boolean changed = manager.changePassword(user, oldPwd, newPwd);
+        boolean changed = manager1.changePassword(user3, oldPwd, newPwd);
 
         assertTrue(changed);
-        verify(dao).updateUser(user);
+        verify(dao).updateUser(user3);
 
         // On ne compare pas avec PasswordHasher.hash(oldPwd) car chaque hash est unique
         assertFalse("Le hash après changement doit être différent de l'ancien",
-                hashedOld.equals(user.getUserPwd()));
+                hashedOld.equals(user3.getUserPwd()));
 
-        assertTrue(PasswordHasher.verify(newPwd, user.  getUserPwd()));
+        assertTrue(PasswordHasher.verify(newPwd, user3.  getUserPwd()));
     }
 
     @Test
